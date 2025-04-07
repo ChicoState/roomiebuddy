68d45d4cec243f8fac67105309285809a5113a0a
AlphaIru <latioslatias2002@gmail.com>
2025-04-07 13:33:37 -0700

    add_delete_edit task

--- backend/log.py
+++ backend/log.py
@@ -11,7 +11,7 @@ from pytz import timezone  # type: ignore
 def make_new_log(log_title: str, log_data: Exception) -> None:
     """Create a log file when an error occurs in the program."""
     with open(
-        "./Yomasete_Iruka_V3/log/log.txt", mode="a", encoding="utf-8"
+        "log/log.txt", mode="a", encoding="utf-8"
     ) as log_file:
         log_file.write(f'{datetime.now(timezone("America/Los_Angeles"))} \n')
         log_file.write(f"In {log_title}:\n")
